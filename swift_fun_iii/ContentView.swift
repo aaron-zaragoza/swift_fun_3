7ca688d26ee8a281bd65bbbff989e00c588de9d1
aaron-zaragoza <76231856+aaron-zaragoza@users.noreply.github.com>
2021-05-18 19:29:12 -0700

    First commit

---
+++ swift_fun_iii/ContentView.swift
@@ -0,0 +1,13 @@
+//
+//  ContentView.swift
+//  swift_fun_iii
+//
+//  Created by Aaron Zaragoza on 5/18/21.
+//
+
+import SwiftUI
+
+// Write a program that adds the numbers 1-255 to an array
+func addToArray(){
+    
+}
---
+++ swift_fun_iii/swift_fun_iiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_fun_iiiApp.swift
+//  swift_fun_iii
+//
+//  Created by Aaron Zaragoza on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_fun_iiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
